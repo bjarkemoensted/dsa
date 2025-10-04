81b3c42ad9158d67b734afd5c0dbb6481af10707
bjarkemoensted <bjarkemoensted@gmail.com>
2025-10-04 14:25:34 +0200

    Cleanup and fixed type hint issues

--- dsa/data_structures/linear/base.py
+++ dsa/data_structures/linear/base.py
@@ -1,20 +1,25 @@
-"""Implements simple linear data structures (stacks, queues)"""
+"""Implements a base class for simple linear data structures (stacks, queues)"""
 
 from abc import ABC, abstractmethod
 from typing import (
-    cast,
+    Any,
     Generic,
     TypeVar
 )
 
-from dsa.data_structures.heap_operations import heappush, heappop
 
-T = TypeVar('T')  # exposed type
+T = TypeVar('T')
 
 DEFAULT_ARR_SIZE = 8
 
 
 class BaseContainer(ABC, Generic[T]):
+    """Base class for linear data structures. Has abstract methods for '_get' and '_put', which can be overridden
+    to implement said operations. Aliases can then be defined for the terminology typically used for the specific data
+    structure, i.e. push/pop for stacks, etc.
+    The _get/_put methods are private to allow more generic public versions (get/put) to run common functionality
+    before inserting/removing elements, such as checking if the data structures is full/empty.
+    In addition, abstract methods _size and _to_list must also be defined for child classes"""
     
     def __init__(self, maxsize: int=-1):
         self.maxsize = maxsize
@@ -47,7 +52,7 @@ class BaseContainer(ABC, Generic[T]):
         raise NotImplementedError
     
     @abstractmethod
-    def _put(self, item: T, **kwargs) -> None:
+    def _put(self, item: T) -> None:
         raise NotImplementedError
     
     @abstractmethod
@@ -57,92 +62,4 @@ class BaseContainer(ABC, Generic[T]):
     @abstractmethod
     def to_list(self) -> list[T]:
         raise NotImplementedError
-    #
-
-
-class Queue(BaseContainer, Generic[T]):
-    """Queue data structure. Implementation follows CLRS section 10.1.
-    The invariants here are the pointers .tail and .head which refer to
-    the next free slot, and the most recently inserted element, respectively (except when the
-    queue is empty)."""
-    
-    def __init__(self, maxsize: int=-1):
-        super().__init__(maxsize=maxsize)
-        
-        initial_size = maxsize + 1 if maxsize != -1 else DEFAULT_ARR_SIZE
-        self.arr = [None for _ in range(initial_size)]
-        self.head = 0
-        self.tail = 0
-
-    def _size(self):
-        return (self.tail - self.head) % len(self.arr)
-    
-    def _grow_array(self):
-        inds = ((self.head + i) % len(self.arr) for i in range(self.size()))
-        vals = [self.arr[i] for i in inds]
-        self.arr = [None for _ in range(2*self.size())]
-        for i, val in enumerate(vals):
-            self.arr[i] = val
-        #
-    
-    def _put(self, item):
-        at_capacity = self.size() == len(self.arr) - 1
-        if at_capacity:
-            self._grow_array()
-        
-        self.arr[self.tail] = item
-        self.tail = (self.tail + 1) % len(self.arr)
-    
-    def _get(self):
-        elem = self.arr[self.head]
-        self.head = (self.head + 1) % len(self.arr)
-        
-        return cast(T, elem)
-    
-    def to_list(self) -> list[T]:
-        inds = ((self.head + i) % len(self.arr) for i in range(self.size()))
-        res = [self.arr[i] for i in inds]
-        return cast(list[T], res)
-    #
-
-    def enqueue(self, item: T):
-        self.put(item)
-    
-    def dequeue(self):
-        return self.get()
-    #
-
-
-def _get_priority(elem: tuple):
-    priority, _ = elem
-    return priority
-
-
-class PriorityQueue(BaseContainer, Generic[T]):
-    def __init__(self, maxsize = -1, stable=True):
-        super().__init__(maxsize)
-        self.stable = stable
-        self.arr = []
-        self._counter = 0
-    
-    def _size(self):
-        return len(self.arr)
-    
-    def _put(self, item, priority=0):
-        if self.stable:
-            priority = (priority, self._counter)
-            self._counter += 1
-        
-        elem = (priority, item)
-        heappush(self.arr, elem, key=_get_priority)
-    
-    def _get(self):
-        _, item = heappop(self.arr, key=_get_priority)
-        return item
-    
-    def to_list(self):
-        if not self.arr:
-            return []
-        _, res = zip(*self.arr)
-        return res
     #
