0c841bad6dd45161611f2a061ca90cb5fba2f45f
bjarkemoensted <bjarkemoensted@gmail.com>
2025-10-04 14:25:34 +0200

    Cleanup and fixed type hint issues

--- dsa/data_structures/linear/priority_queue.py
+++ dsa/data_structures/linear/priority_queue.py
@@ -1,7 +1,11 @@
-from typing import Generic
+from typing import Generic, TypeAlias
 
 from dsa.data_structures.linear.base import BaseContainer, T
 from dsa.data_structures.heap_operations import heappop, heappush
+
+
+prioritytype: TypeAlias = float|tuple[float, int]
+elemtype: TypeAlias = tuple[prioritytype, T]
 
 
 def _get_priority(elem: tuple):
@@ -10,7 +14,16 @@ def _get_priority(elem: tuple):
 
 
 class PriorityQueue(BaseContainer, Generic[T]):
-    def __init__(self, maxsize = -1, stable=True):
+    """Priority queue, using a min-heap under the hood (i.e. elements with lowest priorities
+    are first returned from the queue)."""
+    
+    arr: list[elemtype]
+    
+    def __init__(self, maxsize = -1, stable=True) -> None:
+        """maxsize (int, optional) - max allowed number of elements in the queue. -1 for unlimited size.
+        stable (bool, default: True) - indicates whether the queue is stable. If stable, elements with equal
+            priorities are returned in the order added."""
+
         super().__init__(maxsize)
         self.stable = stable
         self.arr = []
@@ -18,16 +31,16 @@ class PriorityQueue(BaseContainer, Generic[T]):
     
     def _size(self):
         return len(self.arr)
-    
-    def _put(self, item, priority=0):
+
+    def _put(self, item, priority: float=0) -> None:
+        priority_: prioritytype = (priority, self._counter) if self.stable else priority
         if self.stable:
-            priority = (priority, self._counter)
             self._counter += 1
-        
-        elem = (priority, item)
+
+        elem: elemtype = (priority_, item)
         heappush(self.arr, elem, key=_get_priority)
     
-    def _get(self):
+    def _get(self) -> T:
         _, item = heappop(self.arr, key=_get_priority)
         return item
     
