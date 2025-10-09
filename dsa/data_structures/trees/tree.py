022fb660d18ba0b34d9c0ccaa26a16104ca4c4c0
bjarke <bjarkemoensted@gmail.com>
2025-10-09 17:38:07 +0200

    Testing tree nodes against anytree

--- dsa/data_structures/trees/tree.py
+++ dsa/data_structures/trees/tree.py
@@ -5,28 +5,32 @@ from typing import Generic, Iterator, TypeVar
 T = TypeVar("T")
 
 
-def iterate_depth_and_nodes(seed: TreeNode) -> Iterator[tuple[int, TreeNode]]:
-    current = [seed]
-    depth = 0
-    next_ = []
-    while current:
-        for node in current:
-            yield depth, node
-            next_ += node.children
-        
-        current = next_
-        next_ = []
-        depth += 1
+class TreeError(Exception):
+    """For generic tree-related errors"""
+    pass
+
+class LoopError(Exception):
+    """Error for when something would cause a cycle in the tree"""
+    pass
 
 
 class TreeNode(Generic[T]):
-    """A node in a tree structure."""
+    """A node in a tree structure.
+    Updates to the tree structure are triggered exlusively when setting the .parent property of a note.
+    Any methods that affect the tree structure, by e.g. adding a child, must trigger subsequent updates
+    through setting parents of the relevant nodes. This directionality is important to avoid accidentally
+    triggering an infinite recursion where adding a child and parent nodes attempt to update each other."""
     
     def __init__(self, key: T, parent: TreeNode|None=None) -> None:
+        """Make a new node instance.
+        key: The value stored on this node.
+        parent: Optional parent node for this one."""
+
         self.key = key
         self._parent: TreeNode|None = None
-        self.parent = parent
         self.children: list[TreeNode] = []
+
+        self.parent = parent
     
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({repr(self.key)})"
@@ -40,6 +44,12 @@ class TreeNode(Generic[T]):
     
     @parent.setter
     def parent(self, new_parent: TreeNode|None) -> None:
+        """Set the parent of this node, and trigger any necessary updates to adjacent nodes"""
+
+        # Check that setting the parent won't introduce a loop        
+        if new_parent is not None and any(child is new_parent for child in self.iter_bfs()):
+            raise LoopError(f"Can't set parent - {new_parent} is a descendant of {self}")
+
         # Remove this node from the old parent's children
         old_parent = self._parent
         if old_parent is not None:
@@ -50,16 +60,29 @@ class TreeNode(Generic[T]):
         if new_parent is not None:
             new_parent._register_child(self)
     
+    def add_child(self, child: TreeNode) -> None:
+        if any(c is child for c in self.children):
+            raise TreeError(f"{child} is already a child of {self}")
+        child.parent = self
+    
+    def remove_child(self, child: TreeNode) -> None:
+        """Removes a child from this node"""
+        if not any(c is child for c in self.children):
+            raise TreeError(f"{child} is not a child of {self}")
+        child.parent = None
+
     def _register_child(self, node: TreeNode) -> None:
+        """Registers a new child, without triggering any other structural changes"""
         self.children.append(node)
     
     def _unregister_child(self, node: TreeNode) -> None:
+        """Unregisters a child, without triggering any other structural changes"""
         for i in reversed(range(len(self.children))):
             if self.children[i] is node:
                 del self.children[i]
             #
         #
-    
+
     @property
     def is_root(self) -> bool:
         return self._parent is None
@@ -71,10 +94,22 @@ class TreeNode(Generic[T]):
         
         return node
     
-    def iter_down(self) -> Iterator[TreeNode]:
-        """Iterate downwards, depth-first, through child nodes"""
+    def iter_dfs(self) -> Iterator[TreeNode[T]]:
+        """Iterate over child nodes downwards, depth-first"""
         yield self
         for child in self.children:
-            yield from child.iter_down()
+            yield from child.iter_dfs()
+        #
+    
+    def iter_bfs(self) -> Iterator[TreeNode[T]]:
+        """Iterate over child nodes downwards, breadth-first"""
+        queue = [self]
+        next_ = []
+        while queue:
+            for node in queue:
+                yield node
+                next_.extend(node.children)
+            queue = next_
+            next_ = []
         #
     #
--- tests/ds/test_trees.py
+++ tests/ds/test_trees.py
@@ -1,4 +1,7 @@
-from typing import get_args
+from __future__ import annotations
+import anytree  # type: ignore
+import random
+from typing import get_args, Optional, overload
 import unittest
 
 from dsa.data_structures.trees import tree
@@ -6,7 +9,6 @@ from dsa.data_structures.trees import visualization
 
 
 def _make_nodes() -> tree.TreeNode:
-    
     nodes = [tree.TreeNode(c) for c in "abcde"]
     for i in range(1, len(nodes)):
         nodes[i].parent = nodes[i-1]
@@ -18,7 +20,7 @@ def _make_nodes() -> tree.TreeNode:
     return a
 
 
-class QueueTest(unittest.TestCase):
+class NodeTest(unittest.TestCase):
     def setUp(self):
         node = _make_nodes()
         
@@ -33,11 +35,191 @@ class QueueTest(unittest.TestCase):
             s = renderer(self.root_node)
             self.assertIsInstance(s, str)
             # Check that all nodes appear in the rendered tree
-            for node in self.root_node.iter_down():
-                self.assertIn(str(node.key), s)
+            keys = (node.key for node in self.root_node.iter_dfs())
+            for key in keys:
+                self.assertIn(str(key), s)
         
         # Check that all registered renderers are allowed by the type hint
         styles_type_hint = sorted(styles)
         styles_registry = sorted(visualization.TreeRenderer._registry.keys())
         self.assertEqual(styles_type_hint, styles_registry)
     #
+
+    def test_iteration(self):
+        dfs = set(self.root_node.iter_dfs())
+        bfs = set(self.root_node.iter_bfs())
+        self.assertEqual(dfs, bfs)
+    #
+
+
+# Map exceptions from anytree to exceptions thrown here
+_exception_mapping = {
+    anytree.node.exceptions.TreeError: tree.TreeError,
+    anytree.node.exceptions.LoopError: tree.LoopError,
+}
+
+
+class WrappedNode(anytree.Node):
+    """Wrapper class for anytree nodes to have attributes with the same name this library"""
+
+    children: tuple[WrappedNode, ...]
+
+    @property
+    def key(self):
+        return self.name
+    
+    def add_child(self, child: WrappedNode) -> None:
+        self.children += (child,)
+    
+    def remove_child(self, child: WrappedNode) -> None:
+        new_children = tuple(c for c in self.children if c is not child)
+        self.children = new_children
+
+
+class AnytreeComparisonTest(unittest.TestCase):
+    """Tests the node tree class against the existing anytree library.
+    This works by initializing two identical trees in both frameworks,
+    then running a lot of randomized operations on the graph structure of both trees,
+    and checking that the trees exhibit similar behavior, i.e. they maintain identical
+    graph structures, and raise errors in the same cases."""
+
+    n_nodes = 100
+    rs: random.Random
+    seed = 0
+    nodes: list[tree.TreeNode]
+    anynodes: list[WrappedNode]
+
+    def setUp(self):
+        self.rs = random.Random()
+        self.rs.seed(self.seed)
+        self.inds = list(range(self.n_nodes))
+        # Setup nodes for both frameworks
+        self.nodes = [tree.TreeNode(key=val) for val in self.inds]
+        self.anynodes = [WrappedNode(val) for val in self.inds]
+
+        assert len(self.inds) == len(self.nodes) == len(self.anynodes) == self.n_nodes
+
+    @overload
+    def randind(self, n: None=...) -> int: ...
+    @overload
+    def randind(self, n: int) -> tuple[int, ...]: ...
+    def randind(self, n: Optional[int]=None) -> int|tuple:
+        """Returns 1 or more random indices.
+        If n is not specified, returns a single index, as an int.
+        Otherwise, returns a tuple of n indices (samples without replacement)."""
+
+        if n is None:
+            return self.rs.choice(self.inds)
+
+        if n > len(self.inds):
+            raise ValueError
+        
+        inds = [i for i in self.inds]
+        self.rs.shuffle(inds)
+
+        res = tuple(inds[:n])
+        return res
+
+    def check_tree_structure(self):
+        """Checks if the trees are similar in structure.
+        Iterates over all nodes and compares their values in both trees, then
+        compares values of parents and children."""
+
+        for i in range(self.n_nodes):
+            a = self.anynodes[i]
+            b = self.nodes[i]
+            
+            # Compare nodes directly
+            self.assertEqual(a.key, b.key)
+
+            # Compare parents
+            if a.parent is None:
+                self.assertIsNone(b.parent)
+            else:
+                self.assertIsNotNone(b.parent, msg=f"{b} should have {a.parent.key} as parent, but has None")
+                assert b.parent
+                self.assertEqual(a.parent.key, b.parent.key)
+            
+            # Compare children
+            self.assertEqual(len(a.children), len(b.children))
+            for ka, kb in zip(*(sorted(c.key for c in n.children) for n in (a, b))):
+                self.assertEqual(ka, kb)
+            #
+        #
+
+    def _set_random_parent(self) -> None:
+        """Attempts to set a random node as parent of another node"""
+        
+        child_ind, parent_ind = self.randind(n=2)
+        try:
+            self.anynodes[child_ind].parent = self.anynodes[parent_ind]
+        except anytree.node.exceptions.LoopError as e:
+            # If anytree won't allow the parent due to cycle creation, expect an error
+            exc = _exception_mapping[type(e)]
+            with self.assertRaises(exc, msg=f"anytree version failed with: {str(e)}"):
+                self.nodes[child_ind].parent = self.nodes[parent_ind]
+            return
+        self.nodes[child_ind].parent = self.nodes[parent_ind]
+    
+    def _remove_random_parent(self) -> None:
+        """Attempts to remove the parent of a random node"""
+        ind = self.randind()
+        self.anynodes[ind].parent = None
+        self.nodes[ind].parent = None
+
+    def _set_random_child(self) -> None:
+        """Attempts to set a random node as child of another node"""
+        child_ind, parent_ind = self.randind(n=2)
+        try:
+            self.anynodes[parent_ind].add_child(self.anynodes[child_ind])
+        except (anytree.node.exceptions.TreeError, anytree.node.exceptions.LoopError) as e:
+            exc = _exception_mapping[type(e)]
+            with self.assertRaises(exc, msg=f"anytree version failed with: {str(e)}"):
+                self.nodes[parent_ind].add_child(self.nodes[child_ind])
+            return
+        
+        self.nodes[parent_ind].add_child(self.nodes[child_ind])
+
+    def _remove_random_child(self) -> None:
+        """Attempts to remove a random child from a random node."""
+
+        all_child_inds = [i for i, c in enumerate(self.anynodes) if c.parent is not None]
+        # Nothing to do if no nodes have any children
+        if not all_child_inds:
+            return
+
+        # Determine a random node and its parent
+        child_ind = self.rs.choice(all_child_inds)
+        child = self.anynodes[child_ind]
+        parent_ind = next(i for i, n in enumerate(self.anynodes) if n is child.parent)
+
+        # Remove the child nodes
+        self.anynodes[parent_ind].remove_child(self.anynodes[child_ind])
+        self.nodes[parent_ind].remove_child(self.nodes[child_ind])
+
+    def test_manipulations(self):
+        """Do a series of random tree manipulations"""
+
+        # Some random manipulations
+        operations = (
+            self._set_random_parent,
+            self._remove_random_parent,
+            self._set_random_child,
+            self._remove_random_child,
+        )
+        # Be more hesitant to remove connections to ensure the trees don't stay too simple
+        weights = [1, 0.1, 1, 0.2]
+        
+        # Check the trees start out similar
+        self.check_tree_structure()
+
+        for _ in range(1000):
+            f = self.rs.choices(operations, weights=weights)[0]
+            f()
+            self.check_tree_structure()
+        #
+    #
+
+
+if __name__ == "__main__":
+    unittest.main()
