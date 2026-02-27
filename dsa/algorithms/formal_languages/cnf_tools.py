93ab37d43b6f0468d4d7d46c88c0c46fc9d11ed1
Bjarke Moensted <bjarkemoensted@gmail.com>
2026-02-27 11:33:13 +0100

    Started work on CNF conversion

--- dsa/algorithms/formal_languages/cnf_tools.py
+++ dsa/algorithms/formal_languages/cnf_tools.py
@@ -1,9 +1,15 @@
-"""Tooling for converting a grammar to Chomsky Normal Form (CNF) and testing CNF."""
+"""Tooling for converting a grammar to Chomsky Normal Form (CNF) and testing CNF.
+This mainly uses the framework of Lange & Leiß (2009), also used on the Wikipedia page on Chomsky Normal Forms.
+"""
+
+from copy import deepcopy
+from typing import Iterator, Literal, overload
 
 from dsa.algorithms.formal_languages.context_free import (
     get_useless_symbols,
     Grammar,
-    Nonterminal
+    Nonterminal,
+    productiontype
 )
 
 
@@ -44,3 +50,101 @@ def grammar_is_cnf(G: Grammar, allow_empty_string_from_start=True) -> bool:
         #
     
     return True
+
+
+
+@overload
+def _iter_rhs(
+    productions: productiontype, flat: Literal[False] = False
+) -> Iterator[tuple[str|Nonterminal, ...]]: ...
+
+@overload
+def _iter_rhs(
+    productions: productiontype, flat: Literal[True]
+) -> Iterator[str|Nonterminal]: ...
+
+def _iter_rhs(
+    productions: productiontype, flat: bool = False
+) -> Iterator[str|Nonterminal|tuple[str|Nonterminal, ...]]:
+    """Iterate over all productions.
+    If flat is True, iterates over all symbols in all production right-hand-sides.
+    Otherwise (default), iterates over individual productions, i.e. tuples of strings/Nonterminals."""
+
+    for prods in productions.values():
+        for p in prods:
+            if flat:
+                for elem in p:
+                    yield elem
+            else:
+                yield p
+            #
+        #
+    #
+
+
+def _start(start_symbol: Nonterminal, productions: productiontype) -> Nonterminal:
+    """START step - ensure no start symbols occur in any RHS productions.
+    Modifies the production rules in-place and returns the new start symbol"""
+    
+    produced_nts = {symbol for symbol in _iter_rhs(productions, flat=True) if isinstance(symbol, Nonterminal)}
+    # If the current start symbol doesn't occur in an RHS, there's nothing to do
+    if start_symbol not in produced_nts:
+        return start_symbol
+    
+    # Find a new, yet unused start symbol (retains the current if absent from all RHS)
+    new_start = start_symbol
+    suffix = -1
+    while new_start in produced_nts:
+        suffix += 1
+        new_start = Nonterminal(f"{start_symbol}_{suffix}")
+
+    # If introducing a new start symbol S_0, add a rule like S_0 -> S
+    if new_start != start_symbol:
+        productions[new_start] = [(start_symbol,)]    
+
+    return new_start
+
+
+def _term(productions: productiontype) -> None:
+    """TERM step - ensure no rules have nonsolitary terminals"""
+    
+    # TODO implement
+    raise NotImplementedError
+
+
+def _bin(productions: productiontype) -> None:
+    """BIN step - ensure no right-hand-sides have more than 2 non-terminals"""
+    
+    # TODO implement
+    raise NotImplementedError
+
+
+def _del(productions: productiontype) -> None:
+    """DEL step - ensure no nonterminals apart from the start symbol produces the empty string"""
+    
+    # TODO implement
+    raise NotImplementedError
+
+
+def _unit(productions: productiontype) -> None:
+    """UNIT step - ensure no nonterminals produce a single nonterminal"""
+    
+    # TODO implement
+    raise NotImplementedError
+
+
+def chomsky_normal_form(G: Grammar) -> Grammar:
+    """Converts a grammar into an equivalent CNF grammar."""
+
+    productions = deepcopy(G.productions)
+
+    S0 = _start(start_symbol=G.start_symbol, productions=productions)
+    assert all(symbol != S0 for symbol in _iter_rhs(productions, flat=True))
+
+    _term(productions=productions)
+    _bin(productions=productions)
+    _del(productions=productions)
+    _unit(productions=productions)
+
+    res = Grammar(production_rules=productions, start_symbol=S0)
+    return res
