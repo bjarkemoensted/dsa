93d6f09f3742b2cddcb3b78d3f312b2c297dc57a
Bjarke Moensted <bjarkemoensted@gmail.com>
2026-08-12 15:29:50 +0200

    Restructure and started NFA

--- dsa/automata/finite_state_machine.py
+++ dsa/automata/finite_state_machine.py
@@ -1,13 +1,13 @@
+from abc import abstractmethod, ABC
 from dataclasses import dataclass
 
 from typing import Sequence
 
 
 @dataclass
-class FiniteStateMachine[Q, S]:
+class AutomatonBase[Q, S](ABC):
     states: set[Q]
     alphabet: set[S]
-    transitions: dict[tuple[Q, S], Q]
     initial_state: Q
     final_states: set[Q]
 
@@ -22,7 +22,16 @@ class FiniteStateMachine[Q, S]:
         )
 
         return all(requirements)
+
+    @abstractmethod
+    def accepts(self, string: Sequence[S]) -> bool:
+        raise NotImplementedError
     #
+
+
+@dataclass
+class DFA[Q, S](AutomatonBase):
+    transitions: dict[tuple[Q, S], Q]
 
     def accepts(self, string: Sequence[S]) -> bool:
         state = self.initial_state
@@ -33,3 +42,19 @@ class FiniteStateMachine[Q, S]:
 
         return state in self.final_states
     #
+
+
+@dataclass
+class NFA[Q, S](AutomatonBase):
+    transitions: dict[tuple[Q, S], set[Q]]
+
+    def accepts(self, string: Sequence) -> bool:
+        states = {self.initial_state}
+        for character in string:
+            states = set.union(*(self.transitions[(state, character)] for state in states))
+
+        end_states = states.intersection(self.final_states)
+        res = len(end_states) > 0
+        return res
+
+    # TODOÆ epsilon transitions and regex parsing/Thompson's construction
