12fbceea18b8b2257ddfe809c2b40054d801087f
Bjarke Moensted <bjarkemoensted@gmail.com>
2026-08-29 12:43:18 +0200

    Add docstrings and cleanup

--- dsa/automata/regex.py
+++ dsa/automata/regex.py
@@ -1,29 +1,28 @@
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from collections import defaultdict
+from collections.abc import Iterator, Sequence
 from dataclasses import dataclass, field, fields
-from itertools import count
 from functools import singledispatchmethod
-from typing import ClassVar, get_args, Iterator, Literal, Self, Sequence, TypeIs
+from itertools import count
+from typing import ClassVar, Literal, Self, TypeIs, get_args
 
-from dsa.automata.finite_state_machine import NFA, Epsilon, EPSILON
+from dsa.automata.finite_state_machine import EPSILON, NFA, Epsilon
 
 
 class ParseError(Exception):
-    pass
+    """Custom error for when we fail to parse a Regex"""
 
 
 @dataclass
-class BaseNode[T](ABC):
+class ASTBaseNode[T](ABC):
     """Base class for a node in the abstract syntax tree (AST)"""
 
     leaf: ClassVar[bool]
 
-    def __repr__(self) -> str:
-        return "EYY"
-
     @abstractmethod
-    def children(self) -> Iterator[BaseNode]:
+    def children(self) -> Iterator[ASTBaseNode]:
         raise NotImplementedError
 
     @classmethod
@@ -39,15 +38,15 @@ class BaseNode[T](ABC):
         return f"{self.__class__.__name__}"
 
     def display(self, indent: int=0) -> None:
+        """Helper method for displaying the AST"""
         space = indent*' '
         print(f"{space}{self.repr_node()}")
         for child in self.children():
             child.display(indent=indent + 2)
-        #
 
 
 @dataclass
-class Atom[T](BaseNode):
+class LiteralNode[T](ASTBaseNode):
     """Node for a single character in an expression"""
 
     leaf: ClassVar[bool] = True
@@ -56,13 +55,12 @@ class Atom[T](BaseNode):
     def repr_node(self) -> str:
         return str(self.value)
 
-    def children(self) -> Iterator[BaseNode]:
+    def children(self) -> Iterator[ASTBaseNode]:
         yield from ()
-    #
 
 
 @dataclass
-class Empty[T](BaseNode):
+class Empty[T](ASTBaseNode):
     """Special node to represent an empty string"""
 
     leaf: ClassVar[bool] = True
@@ -70,14 +68,13 @@ class Empty[T](BaseNode):
     def repr_node(self) -> str:
         return "ε"
 
-    def children(self) -> Iterator[BaseNode]:
+    def children(self) -> Iterator[ASTBaseNode]:
         yield from ()
-    #
 
 
 
 @dataclass
-class Operator(BaseNode):
+class Operator(ASTBaseNode):
     """Base class for nodes that represent an operation (Kleene star, concatenation, etc)"""
 
     # Associate a precedence with each operation, for shunting yard algorithm
@@ -88,22 +85,20 @@ class Operator(BaseNode):
         super().__init_subclass__(**kwargs)
         cls.precedence = precedence
 
-    def children(self) -> Iterator[BaseNode]:
-        for field in fields(self):
-            assert isinstance(field, BaseNode)
-            yield field
-        #
-    #
+    def children(self) -> Iterator[ASTBaseNode]:
+        for field_ in fields(self):
+            assert isinstance(field_, ASTBaseNode)
+            yield field_
 
 
 @dataclass
 class Concat[T](Operator, precedence=2):
     """Node representing the concatenation operation, e.g. ab (implicit concatenation)"""
 
-    left: BaseNode[T]
-    right: BaseNode[T]
+    left: ASTBaseNode[T]
+    right: ASTBaseNode[T]
 
-    def children(self) -> Iterator[BaseNode]:
+    def children(self) -> Iterator[ASTBaseNode]:
         yield from (self.left, self.right)
 
 
@@ -111,10 +106,10 @@ class Concat[T](Operator, precedence=2):
 class Union[T](Operator, precedence=1):
     """Node representing the union operation, e.g. a|b"""
 
-    left: BaseNode[T]
-    right: BaseNode[T]
+    left: ASTBaseNode[T]
+    right: ASTBaseNode[T]
 
-    def children(self) -> Iterator[BaseNode]:
+    def children(self) -> Iterator[ASTBaseNode]:
         yield from (self.left, self.right)
 
 
@@ -122,24 +117,24 @@ class Union[T](Operator, precedence=1):
 class Star[T](Operator, precedence=3):
     """Node representing the Kleene star, e.g. a*"""
 
-    expr: BaseNode[T]
+    expr: ASTBaseNode[T]
 
-    def children(self) -> Iterator[BaseNode]:
+    def children(self) -> Iterator[ASTBaseNode]:
         yield self.expr
 
 
 
 # Special characters for regular expressions
-type specialchar = Literal["(", ")", "*", "|"]
-_special_chars = set(get_args(specialchar.__value__))
+type SpecialChar = Literal["(", ")", "*", "|"]
+_special_chars = set(get_args(SpecialChar.__value__))
 
 
-def is_special(char: object) -> TypeIs[specialchar]:
+def is_special(char: object) -> TypeIs[SpecialChar]:
     return isinstance(char, str) and (char in _special_chars)
 
 
 # Map symbols to the corresponding operator class
-OPERATOR_SYMBOLS: dict[specialchar, type[Operator]] = {
+OPERATOR_SYMBOLS: dict[SpecialChar, type[Operator]] = {
     "*": Star,
     "|": Union
 }
@@ -149,16 +144,20 @@ class Parser[T]:
     """A regex parser. This is initialized with an expression consisting of any data type T, and
     special characters (parentheses, operations).
     It uses the shunting yard algorithm to transform the expression into postfix (reverse Polish) notation,
-    consisting only of atomic nodes and operations. Then, the AST is constructed from the postfix data."""
+    consisting only of atomic nodes and operations. Then, the AST is constructed from the postfix data.
+    
+    Simple usage:
+    ast = Parser(expr).parse()
+    """
 
-    def __init__(self, expr: Sequence[T|specialchar]) -> None:
-        self.expr: Sequence[T|specialchar] = expr
+    def __init__(self, expr: Sequence[T|SpecialChar]) -> None:
+        self.expr: Sequence[T|SpecialChar] = expr
 
         # Operators and parentheses are stored here (parentheses represented with None)
         self.operators: list[type[Operator]|None] = []
 
         # Output queue for postfix notation
-        self.postfix: list[Atom[T]|type[Operator]] = []
+        self.postfix: list[LiteralNode[T]|type[Operator]] = []
         self.preprocessed = False
 
     def _push_operator(self, operator: type[Operator]) -> None:
@@ -188,7 +187,7 @@ class Parser[T]:
                 if can_concatenate:
                     self._push_operator(Concat)
 
-                self.postfix.append(Atom(token))
+                self.postfix.append(LiteralNode(token))
             elif token == "(":
                 if can_concatenate:
                     self._push_operator(Concat)
@@ -218,24 +217,23 @@ class Parser[T]:
 
         self.preprocessed = True
 
-    def construct_ast(self) -> BaseNode[T]:
+    def construct_ast(self) -> ASTBaseNode[T]:
         """Construct AST from postfix data"""
 
         # Make sure the postfix step has run
         if not self.preprocessed:
             raise RuntimeError
-        stack: list[BaseNode[T]] = []
+        stack: list[ASTBaseNode[T]] = []
 
         for token in self.postfix:
             # Push atomic tokens to the operand stack
-            if isinstance(token, Atom):
+            if isinstance(token, LiteralNode):
                 stack.append(token)
             else:
                 # When encountering an operator, pop the required operands and apply
                 args = tuple(stack.pop() for _ in range(token.n_args()))
                 elem = token(*reversed(args))
                 stack.append(elem)
-            #
 
         # If the expression was valid, the stack has the AST root node as its only element
         res = stack.pop()
@@ -244,7 +242,7 @@ class Parser[T]:
 
         return res
 
-    def parse(self) -> BaseNode[T]:
+    def parse(self) -> ASTBaseNode[T]:
         """Parses regex and returns the AST root node"""
         if len(self.expr) == 0:
             return Empty()
@@ -253,118 +251,143 @@ class Parser[T]:
         return res
 
 
-def regex_to_ast[T](expr: Sequence[T|specialchar]) -> BaseNode[T]:
-    parser = Parser(expr)
-
-    ast = parser.parse()
-    ast.display()  # !!!
-
-    return ast
-
-
 @dataclass
 class Fragment[Q, S]:
+    """Represents a fragment of an NFA.
+    When building an NFA from an AST, we construct many smaller components of the final NFA.
+    Those components have some special properties, such as only having a single final/accept state.
+    Because many of the rules for combining these fragments rely on these properties, construction is greatly
+    simplified by using a special class for fragments during construction, only creating an NFA after all
+    components are combined"""
+
     initial_state: Q
     final_state: Q
     transitions: defaultdict[tuple[Q, S|Epsilon], set[Q]] = field(default_factory=lambda: defaultdict(set))
 
-    def add_transition(self, from_: Q, to_: Q, char: S|Epsilon=EPSILON) -> Self:
-        self.transitions[(from_, char)].add(to_)
+    def add_transition(self, u: Q, v: Q, char: S|Epsilon=EPSILON) -> Self:
+        """Adds a transition from one state to another.
+        If no character is provided, the state is inferred to be an epsilon-transition"""
+        self.transitions[(u, char)].add(v)
         return self
 
-    def to_NFA(self) -> NFA[Q, S]:
-        states = {from_ for from_, _ in self.transitions.keys()} | set.union(*self.transitions.values())
-        alphabet = {char for _, char in self.transitions.keys() if char is not EPSILON}
 
-        nfa = NFA(
-            states=states,
-            initial_state=self.initial_state,
-            final_states={self.final_state,},
-            alphabet=alphabet,
-            transitions=dict(self.transitions)
-        )
+def _NFA_from_fragment[Q, S](fragment: Fragment[Q, S]) -> NFA[Q, S]:
+    """Convert a fragment into an NFA"""
 
-        return nfa
+    # Infer the set of states and the alphabet from the transition rules
+    states = {from_ for from_, _ in fragment.transitions} | set().union(*fragment.transitions.values())
+    alphabet = {char for _, char in fragment.transitions if char is not EPSILON}
+
+    nfa = NFA(
+        states=states,
+        initial_state=fragment.initial_state,
+        final_states={fragment.final_state,},
+        alphabet=alphabet,
+        transitions=dict(fragment.transitions)
+    )
+
+    return nfa
         
 
 class Constructor[Q]:
+    """Helper class for constructing an NFA from an AST.
+    Instances of this class can be called with an AST to obtain the corresponding NFA."""
+
     def __init__(self, node_generator: Iterator[Q]) -> None:
+        """Instantiate a constructor.
+        The node generator is used to generate distinct nodes for the NFA."""
         self.node_generator = node_generator
 
     @singledispatchmethod
-    def build[S](self, ast: BaseNode[S]) -> Fragment[Q, S]:
-        raise NotImplementedError(f"No dispatch method registered for {type(ast)}")
+    def construct_fragment[S](self, node: ASTBaseNode[S]) -> Fragment[Q, S]:
+        """Construct a fragment from a given node in the AST.
+        This is a dispatch method, delegating handling of each node class to its registered handlers.
+        We raise an error if an unregistered node class is encountered"""
+        raise NotImplementedError(f"No dispatch method registered for {type(node)}")
 
-    def make_fragment(self):
+    def empty_fragment(self):
         """Make an empty fragment (with no transition rules)"""
         u = next(self.node_generator)
         v = next(self.node_generator)
         res = Fragment(initial_state=u, final_state=v)
         return res
 
-    @build.register
-    def _(self, ast: Atom):
-        res = self.make_fragment()
-        res.add_transition(from_=res.initial_state, to_=res.final_state, char=ast.value)
+    @construct_fragment.register
+    def _(self, node: LiteralNode):
+        # Construct a fragment for a literal node
+        res = self.empty_fragment()
+        res.add_transition(u=res.initial_state, v=res.final_state, char=node.value)
         return res
 
-    @build.register
-    def _(self, ast: Union):
-        left = self.build(ast.left)
-        right = self.build(ast.right)
+    @construct_fragment.register
+    def _(self, node: Union):
+        # Construct fragments for the left and right sides of the union
+        left = self.construct_fragment(node.left)
+        right = self.construct_fragment(node.right)
 
-        res = self.make_fragment()
+        res = self.empty_fragment()
         res.transitions |= (left.transitions | right.transitions)
 
+        # Run left/right in parallel - connect start and end to both
         for component in (left, right):
             res.add_transition(res.initial_state, component.initial_state)
             res.add_transition(component.final_state, res.final_state)
 
         return res
 
-    @build.register
-    def _(self, ast: Concat):
-        left = self.build(ast.left)
-        right = self.build(ast.right)
+    @construct_fragment.register
+    def _(self, node: Concat):
+        # Construct fragments for the left and right sides of the concatenation
+        left = self.construct_fragment(node.left)
+        right = self.construct_fragment(node.right)
 
+        # Combine the two, adding an epsilon-transition from the left to the right part
         res = Fragment(
             initial_state=left.initial_state,
             final_state=right.final_state,
             transitions=left.transitions | right.transitions
-        )
+        ).add_transition(u = left.final_state, v = right.initial_state)
 
-        res.add_transition(from_ = left.final_state, to_ = right.initial_state)
         return res
 
-    @build.register
-    def _(self, ast: Star):
-        outer = self.make_fragment()
-        inner = self.build(ast.expr)
-        
+    @construct_fragment.register
+    def _(self, node: Star):
+        outer = self.empty_fragment()
+        # Construct fragment for the inner expression (which the Kleene star repeats)
+        inner = self.construct_fragment(node.expr)
+
+        # Combine outer and inner fragment transitions
         outer.transitions |= inner.transitions
 
-        epsilon_transitions = (
-            (outer.initial_state, inner.initial_state),
-            (inner.final_state, inner.initial_state),
-            (inner.final_state, outer.final_state),
-            (outer.initial_state, outer.final_state),
-        )
+        # Make it optional to go into the inner expression at all
+        outer.add_transition(outer.initial_state, inner.initial_state)
+        outer.add_transition(outer.initial_state, outer.final_state)
 
-        for u, v in epsilon_transitions:
-            outer.add_transition(u, v)
+        # At the end of the inner expression, finish, or repeat the expression
+        outer.add_transition(inner.final_state, inner.initial_state)
+        outer.add_transition(inner.final_state, outer.final_state)
 
         return outer
 
-    def __call__[S](self, ast: BaseNode[S]) -> NFA[Q, S]:
-        root_fragment = self.build(ast)
+    @construct_fragment.register
+    def _(self, node: Empty):
+        res = self.empty_fragment()
+        # Add an epsilon-transition from start -> accept
+        res.add_transition(u=res.initial_state, v=res.final_state)
+        return res
 
-        res = root_fragment.to_NFA()
+    def __call__[S](self, node: ASTBaseNode[S]) -> NFA[Q, S]:
+        root_fragment = self.construct_fragment(node)
+        res = _NFA_from_fragment(root_fragment)
         return res
 
 
-def regex_to_NFA[S](expr: Sequence[S]) -> NFA[int, S]:    
-    constructor = Constructor(node_generator=count())
-
+def regex_to_NFA[S](expr: Sequence[S]) -> NFA[int, S]:
+    """Convert a regular expression into an NFA"""
+    
+    # Get the abstract syntax tree
     ast = Parser(expr).parse()
+    # Construct the NFA
+    constructor = Constructor(node_generator=count())
     res = constructor(ast)
     return res
