79bfc36d0cc32a73aa5b721c2ebc49c987bfec84
Bjarke <bjarkemoensted@gmail.com>
2026-09-05 16:41:56 +0200

    Starting sorting stuff

--- dsa/utils/comparison.py
+++ dsa/utils/comparison.py
@@ -1,3 +1,4 @@
+from typing import cast, overload
 
 from dsa.utils.types import Comparison, Conversion
 
@@ -11,3 +12,15 @@ class KeyComparison[T]:
 
     def __call__(self, a: T, b: T, /) -> bool:
         return self.relation(self.key(a), self.key(b))
+
+
+@overload
+def make_comparison[T](relation: Comparison[T], *, key: None=..., **kwargs: object) -> Comparison[T]: ...
+@overload
+def make_comparison[T, C](relation: Comparison[C], *, key: Conversion[T, C], **kwargs: object) -> Comparison[T]: ...
+def make_comparison[T, C](relation: Comparison[C], *, key: Conversion[T, C]|None=None, **kwargs: object) -> Comparison[T]:
+    """Helper function for making a comparer callable, optionally with an intermediate function"""
+    if key is None:
+        return cast(Comparison[T], relation)
+
+    return KeyComparison(relation, key)
